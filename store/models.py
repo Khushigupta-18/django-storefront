5557a912ab1fc83e33720bdce0495a98d73733af
vaishnavi.pandey <vaishnavi.pandey@ciphersquare.tech>
2026-02-04 15:33:49 +0530

    Fix the error with related name

--- store/models.py
+++ store/models.py
@@ -18,7 +18,7 @@ class Product(models.Model):
     title = models.CharField(max_length=255)
     slug = models.SlugField()
     description = models.TextField()
-    unit_price = models.DecimalField(max_digits=6, decimal_places=2)    #999.99
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)    #999.99  # Fix the error with related name
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now=True)
     Collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
