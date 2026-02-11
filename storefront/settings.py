037a707c6361a87e954221811fc78e663765d376
vaishnavi.pandey <vaishnavi.pandey@ciphersquare.tech>
2026-02-11 17:27:48 +0530

    Switch to SQLite for Render

--- storefront/settings.py
+++ storefront/settings.py
@@ -84,15 +84,10 @@ WSGI_APPLICATION = 'storefront.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'storefront',
-        'HOST': 'localhost',
-        'USER': 'root',
-        'PASSWORD': 'Relipay#12@Services',
-        'PORT': '3306'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
